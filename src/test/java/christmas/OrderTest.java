e0362e0872e2b00aae7d59cfcb24cfc0185cfe82
Park Eunhyeong <silbroeh@gmail.com>
2023-11-13 17:05:28 +0900

    test : 주문 메뉴 관련 테스트 refactor

--- src/test/java/christmas/OrderTest.java
+++ src/test/java/christmas/OrderTest.java
@@ -15,16 +15,12 @@ import static christmas.constant.MenuType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
-    private Order order1;
-    private Order order2;
-    private Order order3;
+    private Map<Menu, Integer> menuBoard1;
+    private Map<Menu, Integer> menuBoard2;
+    private Map<Menu, Integer> menuBoard3;
 
     @BeforeEach
     void setUp() {
-        Map<Menu, Integer> menuBoard1;
-        Map<Menu, Integer> menuBoard2;
-        Map<Menu, Integer> menuBoard3;
-
         menuBoard1 = new HashMap<>();
         menuBoard1.put(TAPAS, 1);
         menuBoard1.put(BARBEQUE_LIB, 1);
@@ -32,41 +28,38 @@ public class OrderTest {
         menuBoard1.put(CHOCOLATE_CAKE, 1);
         menuBoard1.put(ZERO_COKE, 1);
         menuBoard1.put(RED_WINE, 1);
-        order1 = new Order(menuBoard1);
 
         menuBoard2 = new HashMap<>();
         menuBoard2.put(T_BONE_STEAK, 1);
         menuBoard2.put(SEAFOOD_PASTA, 2);
         menuBoard2.put(CHAMPAGNE, 2);
-        order2 = new Order(menuBoard2);
 
         menuBoard3 = new HashMap<>();
         Arrays.stream(Menu.values())
                 .forEach(menu -> menuBoard3.put(menu, 1));
-        order3 = new Order(menuBoard3);
     }
 
     @DisplayName("getTotalCount 메소드 사용시 총메뉴 개수 리턴")
     @Test
     void getTotalCount_test() {
-        assertEquals(order1.getTotalCount(),6);
-        assertEquals(order2.getTotalCount(),5);
-        assertEquals(order3.getTotalCount(),12);
+        assertEquals(new Order(menuBoard1).getTotalCount(),6);
+        assertEquals(new Order(menuBoard2).getTotalCount(),5);
+        assertEquals(new Order(menuBoard3).getTotalCount(),12);
     }
 
     @DisplayName("getMenuTypeCount 메소드 사용시 해당 메뉴타입의 개수 리턴")
     @Test
     void getMenuTypeCount_test() {
-        assertEquals(order1.getMenuTypeCount(MAIN),2);
-        assertEquals(order2.getMenuTypeCount(DESERT),0);
-        assertEquals(order3.getMenuTypeCount(APPETIZER),3);
+        assertEquals(new Order(menuBoard1).getMenuTypeCount(MAIN),2);
+        assertEquals(new Order(menuBoard2).getMenuTypeCount(DESERT),0);
+        assertEquals(new Order(menuBoard3).getMenuTypeCount(APPETIZER),3);
     }
 
     @DisplayName("getTotalAmount 메소드 사용시 총주문 금액 리턴")
     @Test
     void getTotalAmount_test() {
-        assertEquals(order1.getTotalAmount(),162500);
-        assertEquals(order2.getTotalAmount(),175000);
-        assertEquals(order3.getTotalAmount(),296500);
+        assertEquals(new Order(menuBoard1).getTotalAmount(),162500);
+        assertEquals(new Order(menuBoard2).getTotalAmount(),175000);
+        assertEquals(new Order(menuBoard3).getTotalAmount(),296500);
     }
 }
